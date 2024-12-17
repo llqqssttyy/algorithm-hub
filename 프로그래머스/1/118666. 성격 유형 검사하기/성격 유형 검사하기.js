e250237c1aa349183faaacda833d09229c8c0b78
KIM DAEUN <midekuna@gmail.com>
2024-12-17 23:53:12 +0900

    성격 유형 검사하기.js 가독성 개선

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,39 +1,16 @@
 function solution(survey, choices) {
-    const SCORES = [undefined, 3, 2, 1, 0, 1, 2, 3];
-    const SLOTS = {
-        R: 0,
-        T: 0,
-        C: 1,
-        F: 1,
-        J: 2,
-        M: 2,
-        A: 3,
-        N: 3,
-    }
-    const characteristics = [
-        { R: 0, T: 0 },
-        { C: 0, F: 0 },
-        { J: 0, M: 0 },
-        { A: 0, N: 0 },
-    ];
-    
-    survey.map((char, idx) => {
+    const characteristics = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+
+    survey.forEach((char, idx) => {
         const [a, b] = char.split("");
         const choice = choices[idx];
-        const character = choice >= 4 ? b : a;
-        const score = Math.abs(4 - choice);
-        const slot = SLOTS[character];
-        
-        const prevScore = characteristics[slot][character];
-        characteristics[slot][character] = prevScore + score;
-    })
-    
-    return characteristics.map((slot) => {
-        const [a, b] = Object.keys(slot);
-        const scoreA = slot[a];
-        const scoreB = slot[b];
-        
-        if (scoreA === scoreB) return Object.keys(slot).sort()[0];
-        return scoreA > scoreB ? a : b
-    }).join("");
+        const score = Math.abs(choice - 4);
+
+        const target = choice > 4 ? b : a;
+        characteristics[target] += score;
+    });
+
+    return ["RT", "CF", "JM", "AN"]
+        .map(([a, b]) => (characteristics[a] >= characteristics[b] ? a : b))
+        .join("");
 }
